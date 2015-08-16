db465dea616f4609ced705b550bbddc22290e366
Roberto Andrade <roberto@cloud.com>
2015-08-16 10:28:04 -0400

    Configuration for meet.cloud.com

---
+++ .config/deploy.sh
@@ -0,0 +1,4 @@
+ssh-copy-id docker@meet.cloud.com
+scp jitsi-meet docker@meet.cloud.com:/mnt/sda1/home/docker/
+scp .ssl/* docker@meet.cloud.com:/mnt/sda1/home/docker/.ssl/
+scp .sip-communicator/* docker@meet.cloud.com:/mnt/sda1/home/docker/.sip-communicator/
